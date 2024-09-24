85aa17a8b582afd17c5a43a228e27ffd8acfd968
catzi <lychen3@asu.edu>
2024-09-24 16:18:11 -0700

    Linda's second commit

--- ASUHelloWorldJavaFXMavenHW3/src/main/java/edu/asu/ASUHelloWorldJavaFXMaven/HelloFX.java
+++ ASUHelloWorldJavaFXMavenHW3/src/main/java/edu/asu/ASUHelloWorldJavaFXMaven/HelloFX.java
@@ -14,9 +14,9 @@ public class HelloFX extends Application {
     public void start(Stage primaryStage) {
     	System.out.println("ASU Hello World!");
     	System.out.println("It started!");
-        primaryStage.setTitle("Linda Chen Hello World Fall 2024");
+        primaryStage.setTitle("In class github activity");
         Button btn = new Button();
-        btn.setText("Display: 'Linda says: Hello World!'");
+        btn.setText("Display: 'Linda's second commit'");
         btn.setOnAction(new EventHandler<>() {
             public void handle(ActionEvent event) {
                 System.out.println("Linda says: Hello World!");
